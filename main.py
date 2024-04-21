6d1583e92bf3534c05f856f3dce6c4a23d2ee8f9
B-teb <112480047+B-teb@users.noreply.github.com>
2024-04-21 19:42:20 +0200

    Finito tabelle

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Ciao")
+import tables.py
+from sqlalchemy import *
+from sqlalchemy.orm import *
+import os.path
+
+def main():
+    if not os.path.exists('data.db'): # Se non esiste il file del database lo crea
+        Base.metadata.create_all(engine)
+
+if __name__ == '__main__':
+    main()
--- tables.py
+++ tables.py
@@ -1,4 +1,7 @@
 from sqlalchemy import *
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+engine = create_engine('sqlite:///data.db', echo=True)
 
 Base = declarative_base()
 
@@ -8,23 +11,131 @@ class Users(Base):
     id = Column(Integer, primary_key=True)
     email = Column(String)
     username = Column(String)
-    passsword = Column(String(256))
+    passsword = Column(String)
     name = Column(String)
     last_name = Column(String)
     user_type = Column(String)
 
     def __repr__(self):
-        return "User ID: {self.id}, email: {self.email}, username: {self.username}, password: {self.passsword}, name: {self.name}, last name: {self.last_name}, user_type: {self.user_type}".format(self.user_type="Vendor" if self.user_type = 'V' else: "Buyer")
+        return "{self.id} {self.email} {self.username} {self.passsword} {self.name} {self.last_name} {self.user_type}"
 
 class Addresses(Base):
     __tablename__ = 'Addresses'
 
     id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey(User.id))
+    user_id = Column(Integer, ForeignKey(Users.id))
     active = Column(Boolean)
     state = Column(String)
     province = Column(String)
 
     def __repr__(self):
-        return "Address id: {self.id}, of the user: {self.user_id}, status: {self.active}, state: {self.state}, province: {self:province}".format(self.active="active" if self.active=True else: "Not active")
+        return "{self.id} {self.user_id} {self.active} {self.state} {self:province}"
+
+class Cart(Base):
+    __tablename__ = 'Cart'
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey(Users.id))
+
+    def __repr__(self):
+        return f"{self.id} {self.user_id}"
+
+class CartProducts(Base):
+    __tablename__ = 'CartProducts'
+
+    cart_id = Column(Integer, primary_key=True, ForeignKey(Cart.id))
+    product_id = Column(Integer, ForeignKey(Products.id))
+    quantity = Column(Integer)
+
+    def __repr__(self):
+        return f"{self.cart_id} {self.product_id} {self.quantity}"
+
+class Products(Base):
+    __tablename__ = 'Products'
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey(Users.id)) # venditore
+    brand = Column(String)
+    product_name = Column(String)
+    date = Column(Date) # Non sicuro del tipo
+    category_id = Column(Integer, ForeignKey(Categories.id))
+    price = Column(Float)
+    availability = Column(Integer)
+    descr = Column(String)
+
+    def __repr__(self):
+        return f"{self.id} {self.product_name} {self.brand} {self.date} {self.category_id} {self.price} {self.availability} {self.descr}"
+
+class Orders(Base):
+    __tablename__ = 'Orders'
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey(Users.id))
+    date = Column(Date)
+    price = Column(Float)
+    address = Column(Integer) # è una Foreign Key alla tabella Addresses
+    payment_method = Column(String)
+    status = Column(String)
+
+    def __repr__(self):
+        return f"{self.id} {self.user_id} {self.date} {self.price} {self.address} {payment_method} {self.status}"
+
+class OrderProducts(Base):
+    __tablename__ = 'OrderProducts'
+
+    order_id = Column(Integer, ForeignKey(Orders.id))
+    product_id = Column(Integer, ForeignKey(Products.id))
+    quantity = Column(Integer)
+
+    def __repr__(self):
+        return f"{self.order_id} {self.product_id} {self.quantity}"
+
+class Categories(Base):
+    __tablename__ = 'Categories'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+
+    def __repr__(self):
+        return f"{self.id} {self.name}"
+
+class Tags(Base):
+    __tablename__ = 'Tags'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+
+    def __repr__(self):
+        return f"{self.id} {self.name}"
+
+class TagProducts(Base):
+    __tablename__ = 'TagProducts'
+
+    tag_id = Column(Integer, primary_key=True, ForeignKey(Tags.id))
+
+class Reviews(Base):
+    __tablename__ = 'Reviews'
+
+    id = Column(Integer, primary_key=True)
+    product_id = Column(Integer, ForeignKey(Products.id))
+    user_id = Column(Integer, ForeignKey(Users.id))
+    review = Column(String)
+
+    def __repr__(self):
+        return f"{self.id} {self.product_id} {self.user_id} {self.review}"
+
+def data_insertion():
+    Session = sessionmaker(engine)
+    with Session as session:
+        session.add_all([Products(),
+                         Products(),
+                         Products(),
+                         Products() ])   
+
+        session.add_all([Categories(),
+                         Categories(),
+                         Categories(),
+                         Categories() ])   
+
+        session.commit()
 
