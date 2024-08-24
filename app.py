30279402a2b401abf1cf6ec341aa89da95b1334f
biipo <filippo@localhost.localdomain>
2024-08-24 21:00:12 +0200

    Fix

--- app.py
+++ app.py
@@ -269,11 +269,6 @@ def reviews_page():
         ).all()
         return render_template('reviews.html', reviews=reviews)
 
-@app.route('/user/<username>')
-def user(username):
-    user = db_session.scalar(select(User).where(User.username == str(username)))
-    return render_template('user.html', user=user)
-
 def clear_cart(user_id):
     db_session.execute(delete(CartProducts).where(CartProducts.user_id == user_id))
 
